d17a0be6b89ebdde1f70b077bd1c728d57fa466c
Cristian Mozetich <cristianmozetich@gmail.com>
2024-02-18 21:04:05 +0000

    simple front

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,38 @@
-import { NavLink } from "react-router-dom"
-import { useContext, useEffect, useState } from "react"
+import { NavLink } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../../utils/ContextProviders";
-import './Header.css'
-
-
+import "./Header.css";
 
 const Header = () => {
-  const[isAdmin, setIsAdmin] = useState(false);
-  const { jwt, decodeToken} = useContext(Context)
+  const [isAdmin, setIsAdmin] = useState(false);
+  const { jwt, decodeToken } = useContext(Context);
 
   useEffect(() => {
-    if(jwt){
-      const token = decodeToken(jwt)
+    if (jwt) {
+      const token = decodeToken(jwt);
 
-      if( token.user.rol === 'user' )
-      setIsAdmin(true)
+      if (token.user.rol === "user") setIsAdmin(true);
     }
-  }, [jwt, decodeToken])
+  }, [jwt, decodeToken]);
 
   return (
     <header className="header">
-        <nav>
-            <ul className="navUl">
-              <div className="navegador">
-                <NavLink to={"/"}>Principal</NavLink>
-                <NavLink to={"/products"}>Productos</NavLink>
-                <NavLink to={"/carts/:id"}>Carrito</NavLink>
-              </div>
-              <div className="registro">
-                <NavLink to={"/register"}>Registro</NavLink>
-                <NavLink to={"/login"}>Login</NavLink>
-                {
-                  isAdmin && 
-                  <NavLink to={"/panel-admin"}>Panel Admin</NavLink>
-                } 
-              </div>
-            </ul>
-        </nav>
+      <nav>
+        <ul className="navUl">
+          <div className="navegador">
+            <NavLink to={"/"}>PRINCIPAL</NavLink>
+            <NavLink to={"/products"}>PRODUCTOS</NavLink>
+            <NavLink to={"/carts/:id"}>MI BOLSA</NavLink>
+          </div>
+          <div className="registro">
+            <NavLink to={"/register"}>REGISTRARME</NavLink>
+            <NavLink to={"/login"}>LOGIN</NavLink>
+            {isAdmin && <NavLink to={"/panel-admin"}>Panel Admin</NavLink>}
+          </div>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
     <div className='home'>
         <div className='home_desc'>
           <img className="img_home" src="/img/simple.webp" alt="mates" />
-          <h1 className='titulo'>Simple</h1>
-          <Link to={"/login"} ><button className='btn btn-dark m-2'>Login</button></Link>
-          <Link to={"/register"} ><button className='btn btn-dark m-2'>Registro</button></Link>
+          <h1 className='titulo'>SIMPLE</h1>
+          <Link className='loginbtn' to={"/login"}>LOGIN</Link>
+          <Link className='register' to={"/register"}>REGISTRARME</Link>
         </div>
     </div>
   )
